6332ea74e90a0d2973b173a214d1113752630e34
mokselb1488 <bat-chat@ukr.net>
2021-02-20 01:11:09 +0200

    fix #32

--- test_list.py
+++ test_list.py
@@ -11,8 +11,8 @@ class TestListMethods(unittest.TestCase):
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
 
-    def test_illia_kripaka_fi_94_2(self):
-        self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
+    def test_maksym_orzhahivsky_fi_93(self):
+        self.assertEqual(sum([1,2,3])/len([1,2,3]),1
 
 
 if __name__ == '__main__':
